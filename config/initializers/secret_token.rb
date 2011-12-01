ef07128db929890840253ca28c7927dba1b5f26c
Ameya Gholkar <ameya.moses@gmail.com>
2011-12-01 18:20:31 -0500

    Initialize Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSC554::Application.config.secret_token = '64e2b417e1bc66e5a72b1cbda5e928128114505c24d92f45c3fbdd6dccce93c7b473e99cc34f9f0f1333db8b6b643e85acd5699d910389195ff1c9cf749404d3'
