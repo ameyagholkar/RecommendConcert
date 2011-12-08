d0749ac37431ebec21c5c7797f116be2acf6654a
unknown <themightytruk@.(none)>
2011-12-08 18:12:04 -0500

    Users who have already got recommendations are deriected to recommendations from the survey page now.

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -1,6 +1,11 @@
 class SurveyController < ApplicationController
   def index
     #Get 5 random artists
+
+    if Recommendation.find_by_user_id(session[:id])
+      redirect_to "/recommendations"
+    end
+
     @artists = Artist.all( :order => "RANDOM()", :limit => 5)
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,15 @@ CSC554::Application.routes.draw do
 
   resources :recommendations
 
-  resources :liked_genres
+  #resources :liked_genres
 
-  resources :users_artist_ratings
+  #resources :users_artist_ratings
 
-  resources :concerts
+  #resources :concerts
 
-  resources :venues
+  #resources :venues
 
-  resources :artists
+  #resources :artists
 
   resources :users
   resources :survey
@@ -71,7 +71,7 @@ CSC554::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'users#index'
+   root :to => 'survey#index'
 
   # See how all your routes lay out with "rake routes"
 
