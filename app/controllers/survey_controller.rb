8ae605e9c8d61b87ae54e55cc3bef1ed29dc7ca3
Mohanish <mohanish@mohanish-Studio-1558.(none)>
2011-12-10 17:08:17 -0500

    Check for existence os session id in survey controller

--- app/controllers/survey_controller.rb
+++ app/controllers/survey_controller.rb
@@ -2,8 +2,10 @@ class SurveyController < ApplicationController
   def index
     #Get 5 random artists
 
-    if Recommendation.find_by_user_id(session[:id]) and !params[:survey_again]
-      redirect_to "/recommendations"
+    if session[:id]
+      if Recommendation.find_by_user_id(session[:id]) and !params[:survey_again]
+        redirect_to "/recommendations"
+      end
     end
 
     @artists = Artist.all( :order => "RANDOM()", :limit => 5)
